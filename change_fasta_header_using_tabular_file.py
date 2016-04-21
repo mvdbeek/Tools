9f68e8c69c86675b9e1932581da7ebc791b3fd16
portiahollyoak <portia@hollyoak.org.uk>
2016-04-21 18:33:18 +0200

    Remove comments and fix argparse

--- change_fasta_header_using_tabular_file.py
+++ change_fasta_header_using_tabular_file.py
@@ -1,18 +1,8 @@
 
 # coding: utf-8
-
-# In[56]:
-
-get_ipython().system('wget --output-document TE_ID_Names.tsv https://mississippi.snv.jussieu.fr/datasets/abd2998095546994/display?to_ext=gg')
-get_ipython().system('wget --output-document TE_seq_d.fasta https://mississippi.snv.jussieu.fr/datasets/ee837b421679e431/display?to_ext=fasta')
-
-
-# In[37]:
-
 import argparse
+import sys
 
-
-# In[57]:
 
 def get_dict(te_name_file_handle):  # In this function, the file te_name_file_handle is used
     """
@@ -28,13 +18,13 @@ def get_dict(te_name_file_handle):  # In this function, the file te_name_file_ha
     return dictionary  # Show dictionary on the screen
 
 
-# In[58]:
-
 def replace_id(line, dictionary):  
 # In this function, the dictionary and the lines of the te_name_file_handle are fed in.
     """
-    This function reads a fasta header and replaces the ID name with Transposon name used in
-    the Flybase Transposon Sequence Set
+    This function reads a fasta header (line), recovers the name of the sequence "(>fasta_1)"
+    and stores this in key. We look up key in the dictionary, and if the key is present,
+    we replace the key in the line with the value that is assigned to the key in the
+    dictionary.
     """
     key = line[1:].strip()  #The key is the ID name (not including the >)
     if key in dictionary:  #If key is in the dictionary, it is replaced
@@ -42,10 +32,7 @@ def replace_id(line, dictionary):
         return line
     print("Transposon %s is not present in reference genome annotation" % key)
     return line
-    
 
-
-# In[61]:
 
 description = ( "This script will exchange TE ID names with their common usage names")
 
@@ -55,21 +42,22 @@ parser.add_argument("input2", help="A multifasta file containing TE ID Names and
 parser.add_argument("output", help="Name of the output fasta file.")
 # uncomment the next line only when interactively testing!
 #args = parser.parse_args(["TE_ID_Names.tsv", "TE_seq_d.fasta", "my_fancy_new_out.fasta"])
-args.parse_args()
+args = parser.parse_args()
 
 
-# In[63]:
+python_version = sys.version_info
+if python_version.major >= 3:
+    kwargs = {"encoding": "utf-8"}
+else:
+    kwargs = {}
 
-with open(args.input1, encoding = "utf-8") as te_name_file_handle:
-         dictionary = get_dict(te_name_file_handle)
+with open(args.input1, **kwargs) as te_name_file_handle:
+    dictionary = get_dict(te_name_file_handle)
 
-
-# In[64]:
-
-with open(args.input2, encoding = "utf-8") as te_seq_file_handle:
+with open(args.input2, **kwargs) as te_seq_file_handle:
     with open(args.output, "w") as output:
         for line in te_seq_file_handle:
-            if line.startswith(">"):
+           if line.startswith(">"):
                 line = replace_id(line, dictionary)
-            output.write(line)
+           output.write(line)
 
