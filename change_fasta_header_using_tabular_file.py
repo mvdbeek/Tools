a4b2bd0e02ca8d3e35dabf1e2b8a374ff9299099
portiahollyoak <portia@hollyoak.org.uk>
2016-04-22 09:50:59 +0200

    change comments and input names to be more generic

--- change_fasta_header_using_tabular_file.py
+++ change_fasta_header_using_tabular_file.py
@@ -4,22 +4,22 @@ import argparse
 import sys
 
 
-def get_dict(te_name_file_handle):  # In this function, the file te_name_file_handle is used
+def get_dict(tab_file_handle):  # In this function, the file tab_file_handle is used
     """
     This function creates a dictionary out of the file provided containing TE ID names\
     and their common usage names separated by a tab
     """
-    dictionary = {}  # A dictionary is named 'dictionary'
-    for line in te_name_file_handle:  # For every line in the file
+    dictionary = {}  # A new dictionary is named 'dictionary'
+    for line in tab_file_handle:  # For every line in the file
         line = line.strip()  # The leading and trailing white spaces are stripped
         key, value = line.split("\t")  # The line is then split where there is a tab and 
                                        # then the two results are defined as key and value
-        dictionary[key] = value  # The key(ID Name) is linked to the value(common use name)
+        dictionary[key] = value  # The key is linked to the value
     return dictionary  # Show dictionary on the screen
 
 
 def replace_id(line, dictionary):  
-# In this function, the dictionary and the lines of the te_name_file_handle are fed in.
+# In this function, the dictionary and the lines of the fasta_file_handle are fed in.
     """
     This function reads a fasta header (line), recovers the name of the sequence "(>fasta_1)"
     and stores this in key. We look up key in the dictionary, and if the key is present,
@@ -30,15 +30,15 @@ def replace_id(line, dictionary):
     if key in dictionary:  #If key is in the dictionary, it is replaced
         line = line.replace(key,dictionary[key])
         return line
-    print("Transposon %s is not present in reference genome annotation" % key)
+    print("Value %s is not present in tabular file" % key)
     return line
 
 
-description = ( "This script will exchange TE ID names with their common usage names")
+description = ( "This script will exchange fasta headers with linked values from a tabular file")
 
 parser = argparse.ArgumentParser(description)
-parser.add_argument("input1", help="A file containing TE ID Names and their common usage names separated by a tab")
-parser.add_argument("input2", help="A multifasta file containing TE ID Names and their sequences")
+parser.add_argument("input1", help="A tabular file containing a list of two values separated by a tab")
+parser.add_argument("input2", help="A multifasta file containing fasta headers and their sequences")
 parser.add_argument("output", help="Name of the output fasta file.")
 # uncomment the next line only when interactively testing!
 #args = parser.parse_args(["TE_ID_Names.tsv", "TE_seq_d.fasta", "my_fancy_new_out.fasta"])
@@ -51,12 +51,12 @@ if python_version.major >= 3:
 else:
     kwargs = {}
 
-with open(args.input1, **kwargs) as te_name_file_handle:
-    dictionary = get_dict(te_name_file_handle)
+with open(args.input1, **kwargs) as tab_file_handle:
+    dictionary = get_dict(tab_file_handle)
 
-with open(args.input2, **kwargs) as te_seq_file_handle:
+with open(args.input2, **kwargs) as fasta_file_handlee:
     with open(args.output, "w") as output:
-        for line in te_seq_file_handle:
+        for line in fasta_file_handle:
            if line.startswith(">"):
                 line = replace_id(line, dictionary)
            output.write(line)
